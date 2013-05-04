494966b51a993b5729d93c800da55f94274ea2a7
Jinghao Shi <jhshi89@gmail.com>
2013-05-04 14:32:59 -0400

    fix indent

--- revision.rb
+++ revision.rb
@@ -3,75 +3,75 @@ require 'jekyll/post'
 
 module Jekyll
 
-    class Post
-        alias_method :original_to_liquid, :to_liquid
+  # add post.file_name and post.full_path
+  class Post
+    alias_method :original_to_liquid, :to_liquid
 
-        def to_liquid
-            file_name = @name
-            full_path = File.join(@base, file_name)
-            original_to_liquid.deep_merge({
-                'full_path' => full_path, 
-                'file_name' => file_name
-            })
+    def to_liquid
+      file_name = @name
+      full_path = File.join(@base, file_name)
+      original_to_liquid.deep_merge({
+        'full_path' => full_path, 
+        'file_name' => file_name
+      })
+    end
+  end
+
+  class RevisionTag < Liquid::Tag
+    DEFAULT_LIMIT = 5
+
+    def initialize(name, marker, token)
+      @params = Hash[*marker.split(/(?:: *)|(?:, *)/)]
+      if @params['limit'] != nil
+        /\d*/.match(@params['limit']) do |m|
+          @limit = m[0].to_i
         end
+      else
+        @limit = DEFAULT_LIMIT
+      end
+      super
     end
 
-    class RevisionTag < Liquid::Tag
-        DEFAULT_LIMIT = 5
+    def render(context)
+      site = context.environments.first['site']
+      if context.environments.first['post'] != nil
+        post_or_page = context.environments.first['post']
+      else
+        post_or_page = context.environments.first['page']
+      end
 
-        def initialize(name, marker, token)
-            @params = Hash[*marker.split(/(?:: *)|(?:, *)/)]
-            if @params['limit'] != nil
-                /\d*/.match(@params['limit']) do |m|
-                    @limit = m[0].to_i
-                end
-            else
-                @limit = DEFAULT_LIMIT
-            end
-            super
+      full_path = post_or_page['full_path']
+
+      cmd = 'git log --date=local --pretty="%cd|%s" --max-count=' + @limit.to_s + ' ' + full_path
+      logs = `#{cmd}`
+
+      html = '<ul>'
+      logs.each_line do |line|
+        parts = line.split('|')
+        date, msg = parts[0], parts[1..-1].join
+        html << '<li><strong>' + date + '</strong><br/>' + msg + '</li>'
+      end
+      html << '</ul>'
+
+      if site['github_user'] != nil && site['github_repo'] != nil 
+        cmd = 'git rev-parse --abbrev-ref HEAD'
+        # chop last '\n' of branch name
+        branch = `#{cmd}`.chop
+        if site['source'] != nil
+          # for Octopress sites
+          link = File.join('https://github.com', site['github_user'], site['github_repo'], 
+                           'commits', branch, site['source'], '_posts', post_or_page['file_name'])
+        else 
+          # for Jekyll sites
+          link = File.join('https://github.com', site['github_user'], site['github_repo'], 
+                           'commits', branch, '_posts', post_or_page['file_name'])
         end
+        html << 'View on <a href=' + link + ' target=_blank>Github</a>'
+      end
 
-        def render(context)
-            site = context.environments.first['site']
-            if context.environments.first['post'] != nil
-                post_or_page = context.environments.first['post']
-            else
-                post_or_page = context.environments.first['page']
-            end
-
-            full_path = post_or_page['full_path']
-
-            cmd = 'git log --date=local --pretty="%H|%cd|%s" --max-count=' + @limit.to_s + ' ' + full_path
-            logs = `#{cmd}`
-
-            html = '<ul>'
-            logs.each_line do |line|
-                parts = line.split('|')
-                hash = parts[0]
-                date = parts[1]
-                msg = parts[2..-1].join
-                html << '<li><strong>' + date + '</strong><br/>' + msg + '</li>'
-            end
-            html << '</ul>'
-
-            if site['github_user'] != nil && site['github_repo'] != nil 
-                cmd = 'git rev-parse --abbrev-ref HEAD'
-                # chop last '\n'
-                branch = `#{cmd}`.chop
-                if site['source'] != nil
-                    source = site['source']
-                    link = File.join('https://github.com', site['github_user'], site['github_repo'], 
-                                     'commits', branch, source, '_posts', post_or_page['file_name'])
-                else 
-                    link = File.join('https://github.com', site['github_user'], site['github_repo'], 
-                                     'commits', branch, '_posts', post_or_page['file_name'])
-                end
-               html << 'View on <a href=' + link + ' target=_blank>Github</a>'
-            end
-
-            return html
-        end
+      return html
     end
+  end
 end
 
 Liquid::Template.register_tag('revision', Jekyll::RevisionTag)
