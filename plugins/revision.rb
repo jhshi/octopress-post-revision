3653865b506b6d3e8d19a2211f09d760bf3bd383
Luke Korth <luke+github@lukekorth.com>
2014-06-24 17:58:45 -0700

    Update date formatting
    Use jekyll-date-format to format revision date using _config.yml date_format value

--- plugins/revision.rb
+++ plugins/revision.rb
@@ -1,3 +1,5 @@
+require 'jekyll-date-format'
+
 module Jekyll
 
   class PostFullPath < Generator
@@ -77,7 +79,8 @@ module Jekyll
       logs.each_line do |line|
         parts = line.split('|')
         date, msg = parts[0], parts[1..-1].join('|') # keep origin pileline from logs
-        html << '<li><strong>' + date + '</strong><br/>' + msg + '</li>'
+        formatted_date = Jekyll::DateFormat.format_date(date, site['date_format'])
+        html << '<li><strong>' + formatted_date + '</strong><br/>' + msg + '</li>'
       end
       html << '</ul>'
 
